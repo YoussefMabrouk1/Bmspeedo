39b3c8277c0f0abe4f6978089b0c061133c22cae
YoussefMabrouk1 <108554272+YoussefMabrouk1@users.noreply.github.com>
2024-09-10 15:31:30 +0300

    speedo proj

---
+++ Speedo Transfer/PersonalInfoVC.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalInfoVC.swift
+//  Speedo Transfer
+//
+
+import UIKit
+
+class PersonalInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.style = .editor
+        title = "Profile Information"
+    }
+    
+
+}
